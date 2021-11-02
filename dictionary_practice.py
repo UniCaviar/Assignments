386d1aff5ce65524e7c4c2bb08fdff25358b05bf
UniCaviar <Justinm2000@outlook.com>
2021-11-02 11:08:40 -0700

    updated dictionary and practice

---
+++ dictionary_practice.py
@@ -0,0 +1,20 @@
+grocery_items={"Chicken":"$1.59", "Beef":"$1.99", "Cheese":"$1.00", "Milk":"$2.50"}
+
+print(grocery_items["Chicken"])
+print(grocery_items["Beef"])
+print(grocery_items["Cheese"])
+print(grocery_items["Milk"])
+
+
+video_games={"Persona 5 Royal":"$59.99", "Persona 4 Golden":"$19.99", "Legend of Zelda Breath of the Wild":"$59.99", "Grand Theft Auto 5":"$29.99"}
+
+print(video_games["Persona 5 Royal"])
+print(video_games["Persona 4 Golden"])
+print(video_games["Legend of Zelda Breath of the Wild"])
+print(video_games["Grand Theft Auto 5"])
+
+shoe_sales={"Jordan 13": 1, "Yeezy": 8, "Foamposite": 10, "Air Max": 5, "SB Dunk": 20}
+shoe_sales.update({"SB Dunk":22})
+shoe_sales.update({"Yeezy":7})
+print(shoe_sales)
+
