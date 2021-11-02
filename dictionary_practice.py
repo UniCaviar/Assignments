18a2bbc1a026942d006e9f937b71f8da0ae25d57
UniCaviar <Justinm2000@outlook.com>
2021-11-02 12:08:54 -0700

    updated dictionary practice

--- dictionary_practice.py
+++ dictionary_practice.py
@@ -1,10 +1,12 @@
+from types import GeneratorType
+
+
 grocery_items={"Chicken":"$1.59", "Beef":"$1.99", "Cheese":"$1.00", "Milk":"$2.50"}
 
 print(grocery_items["Chicken"])
 print(grocery_items["Beef"])
 print(grocery_items["Cheese"])
 print(grocery_items["Milk"])
-
 
 video_games={"Persona 5 Royal":"$59.99", "Persona 4 Golden":"$19.99", "Legend of Zelda Breath of the Wild":"$59.99", "Grand Theft Auto 5":"$29.99"}
 
@@ -13,8 +15,47 @@ print(video_games["Persona 4 Golden"])
 print(video_games["Legend of Zelda Breath of the Wild"])
 print(video_games["Grand Theft Auto 5"])
 
-shoe_sales={"Jordan 13": 1, "Yeezy": 8, "Foamposite": 10, "Air Max": 5, "SB Dunk": 20}
-shoe_sales.update({"SB Dunk":22})
-shoe_sales.update({"Yeezy":7})
-print(shoe_sales)
+shoe_names={"Jordan 13": 1, "Yeezy": 8, "Foamposite": 10, "Air Max": 5, "SB Dunk": 20}
+shoe_names.update({"SB Dunk":22})
+print(shoe_names["SB Dunk"])
+shoe_names.update({"Yeezy":7})
+print(shoe_names["Yeezy"])
+
+shoe_names.update({"Jordan 13":8})
+shoe_names.update({"Yeezy":14})
+shoe_names.update({"Foamposite":17})
+shoe_names.update({"Air Max":12})
+shoe_names.update({"SB Dunk":29})
+print(shoe_names)
+
+shoe_names.update({"Jordan 13":3})
+shoe_names.update({"Yeezy":11})
+shoe_names.update({"Foamposite":14})
+shoe_names.update({"Air Max":9})
+shoe_names.update({"SB Dunk":26})
+print(shoe_names)
+
+grocery_items["Bread"]="$3.99"
+grocery_items["Cereal"]="$4.99"
+grocery_items["Banana"]="$1.50"
+
+video_games["Persona 3 FES"]= "$29.99"
+video_games["Grand Theft Auto The Trilogy"]="59.99"
+video_games["Pokemon Brilliant Diamond"]="$59.99"
+
+shoe_names["Low 1"]=10
+shoe_names["Capri Onyx"]=5
+shoe_names["Air Force 1"]=15
+
+del grocery_items["Chicken"]
+del grocery_items["Cheese"]
+print(grocery_items)
+
+del video_games["Pokemon Brilliant Diamond"]
+del video_games["Persona 3 FES"]
+print(video_games)
+
+del shoe_names["Air Max"]
+del shoe_names["Jordan 13"]
+print(shoe_names)
 
---
+++ list_practice.py
@@ -0,0 +1,31 @@
+city_names=["Oakland", "Atlanta", "New York City", "Seatle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
+print(city_names)
+
+dog_breeds=["Bulldog", "Labrador Retriever", "Poodle", "German Shepherd", "Golden Retriever", "Siberian Husky", "Dachshund", "Chihuahua", "Pomeranian", "Yorkshire Terrier"]
+print(dog_breeds)
+
+city_names[7]
+city_names[5]
+city_names[0]
+
+dog_breeds[3]
+dog_breeds[4]
+dog_breeds[1]
+
+favorite_city=city_names[0:3]
+favorite_dog_breeds=dog_breeds[3, 4, 1, 0]
+
+city_names[0]="San  Francisco"
+city_names[2]="Brookyln"
+city_names[7]="Hollywood"
+city_names[5]="Tampa"
+
+city_names.append("Oakland")
+city_names.extend(["New York City", "Los Angeles"])
+city_names.insert(0, "Miami")
+print(city_names)
+
+del city_names[3]
+city_names.pop(5)
+city_names.remove("Miami")
+print(city_names)
