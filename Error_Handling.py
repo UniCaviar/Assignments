fdac8c643554d4e653ed07f49a4f466a9af51c17
UniCaviar <Justinm2000@outlook.com>
2021-11-30 12:15:33 -0800

    Error Handling Finished

---
+++ Error_Handling.py
@@ -0,0 +1,16 @@
+"""
+We're writing a program that adds two numbers together. 
+Test this and solve any errors you find if you can. 
+Solving one problem might turn up a new error...
+Be sure to keep a note of whatever errors you find!
+NOTE 1: T in "print" is missing
+NOTE 2: there is "_" in number_2 when it should be "number2"
+NOTE 3: line 12 didn't have a " at the end of the string.
+"""
+
+
+print("Welcome to the addition helper")
+number1 = int(input("Please enter the first number:"))
+number2 = int(input("Please enter the second number: "))
+
+print("The result is: ", number1 + number2)
